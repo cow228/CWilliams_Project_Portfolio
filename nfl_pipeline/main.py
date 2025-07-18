8cc55a04795e0827876abd1fdc1d82988ff15b43
cow228 <cowilliams228@gmail.com>
2025-07-18 13:18:57 -0500

    change file structure

---
+++ nfl_pipeline/main.py
@@ -0,0 +1,30 @@
+'''
+Find feature corelations to target
+'''
+import feature_imp_cl
+import CWilliams_Project_Portfolio.Pipeline_v2.Preprocessing.load_data as load_data
+import subprocess
+
+if __name__ == '__main__':
+    import argparse
+
+    # input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='path to CSV')
+    parser.add_argument('target', help='column name of target feature')
+    parser.add_argument('process', help='t/f if you want to preprocess the data')
+    args = parser.parse_args()
+
+    # perform preprocessing if necessary
+    path = args.path
+    if args.process:
+        subprocess.run(['python','preprocessing.py '+args.path])
+        path = args.path+'_procd'
+
+    # load data
+    data, feature_lists, feature_types = load_data.load_file(path)
+    
+    # feature correlation
+    data_fts = feature_imp_cl.Data_features(data, args.target)
+    data_fts.plot_target_correlation()
+    
